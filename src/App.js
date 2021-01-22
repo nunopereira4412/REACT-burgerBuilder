fa98ffdfc7282ff6cbf3a0a4cd5cb6a46ee2d237
Nuno Pereira <nuno.pereira4412@gmail.com>
2021-01-22 14:47:58 +0000

    Added routes guard for unauthenticated users

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {Component}  from 'react';
-import {Route, Switch}     from 'react-router-dom';
+import {Route, Switch, Redirect}     from 'react-router-dom';
 
 import Layout              from './hoc/Layout/Layout';
 import BurgerBuilder       from './containers/BurgerBuilder/BurgerBuilder';
@@ -18,24 +18,44 @@ class App extends Component {
   }
 
   render() {
+
+    let routes = (
+      <Switch>
+          <Route path="/auth" component={Auth}/>
+          <Route path="/" exact component={BurgerBuilder}/>
+          <Redirect to="/"/>
+      </Switch>
+    );
+    
+    if(this.props.isLoggedIn) 
+      routes = (
+        <Switch>
+              <Route path="/checkout" component={Checkout}/>
+              <Route path="/orders" component={Orders}/>
+              <Route path="/auth" component={Auth}/>
+              <Route path="/logout" component={Logout}/>
+              <Route path="/" exact component={BurgerBuilder}/>
+              <Redirect to="/"/>
+          </Switch>
+      );
+
     return (
       <Layout>
-        <Switch>
-          <Route path="/checkout" component={Checkout}/>
-          <Route path="/orders" component={Orders}/>
-          <Route path="/auth" component={Auth}/>
-          <Route path="/logout" component={Logout}/>
-          <Route path="/" exact component={BurgerBuilder}/>
-        </Switch>
+        {routes}
       </Layout>
     );
   }
 }
 
+const mapStateToProps = state => {
+  return {
+    isLoggedIn: state.auth.isLoggedIn
+  };
+};
 const mapDispatchToProps = dispatch => {
   return {
     checkAuthState: () => dispatch(actionCreators.checkAuthState())
   };
 };
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
